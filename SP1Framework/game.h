3824dddc59b78f607adf6e0f685f7686cb5670fe
7Seven-Eleven11 <50125779+7Seven-Eleven11@users.noreply.github.com>
2020-08-18 12:01:10 +0800

    Updated game.h to include bullets

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -50,6 +50,11 @@ struct SGameChar
     bool  m_bActive;
 };
 
+struct SBulletChar
+{
+    COORD bulletLocation;
+};
+
 void init        ( void );      // initialize your variables, allocate memory, etc
 void getInput    ( void );      // get input from player
 void update      ( double dt ); // update the game and the state of the game
@@ -59,11 +64,13 @@ void shutdown    ( void );      // do clean up, free memory
 void splashScreenWait();    // waits for time to pass in splash screen
 void updateGame();          // gameplay logic
 void moveCharacter();       // moves the character, collision detection, physics, etc
+void shootBullet();         // Shoots a bullet from player position;
 void processUserInput();    // checks if you should change states or do something else with the game, e.g. pause, exit
 void clearScreen();         // clears the current screen and draw from scratch 
 void renderSplashScreen();  // renders the splash screen
 void renderGame();          // renders the game stuff
 void renderMap();           // renders the map to the buffer first
+void renderBullet();        // renders the bullet
 void renderCharacter();     // renders the character into the buffer
 void renderFramerate();     // renders debug information, frame rate, elapsed time, etc
 void renderToScreen();      // dump the contents of the buffer to the screen, one frame worth of game
