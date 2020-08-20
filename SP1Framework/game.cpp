80994bdba3d37d46a06c519e87d4738873023709
Yu Yang <tanyuyang2@gmail.com>
2020-08-20 12:54:28 +0800

    Update game.cpp

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -512,8 +512,8 @@ void renderSplashScreenLevel1()  // renders the splash screen
     clearScreen();
     COORD c = g_Console.getConsoleSize();
     c.Y /= 3;
-    c.X = c.X / 2 - 5;
-    g_Console.writeToBuffer(c, "STAGE 1", 0x03);
+    c.X = c.X / 2 - 6;
+    g_Console.writeToBuffer(c, "STAGE 1 - 1", 0x03);
     c.Y += 1;
     c.X = g_Console.getConsoleSize().X / 2 - 25;
     g_Console.writeToBuffer(c, "Press <Space> to fire and change character colour", 0x09);
