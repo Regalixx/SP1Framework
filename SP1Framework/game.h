7a75255fcea86ba9e54dd707212502dd4286c647
Ast0lf02109 <69616994+Ast0lf02109@users.noreply.github.com>
2020-08-25 12:31:52 +0800

    Update game.h

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -100,6 +100,7 @@ void renderFire();
 void renderFramerate();     // renders debug information, frame rate, elapsed time, etc
 void renderToScreen();      // dump the contents of the buffer to the screen, one frame worth of game
 void renderInputEvents();   // renders the status of input events
+void renderName();
 
 // keyboard and mouse input event managers
 void keyboardHandler(const KEY_EVENT_RECORD& keyboardEvent);  // define this function for the console to call when there are keyboard events
