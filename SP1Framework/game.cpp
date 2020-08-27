0bf92d0843628121b91ac587a95254f0c9c502d8
Ast0lf02109 <69616994+Ast0lf02109@users.noreply.github.com>
2020-08-27 10:52:25 +0800

    Update game.cpp

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -296,7 +296,7 @@ void update(double dt)
 
 void splashScreenWait()    // waits for time to pass in splash screen
 {
-    if (g_dElapsedTime > 6.0) // wait for 3 seconds to switch to game mode, else do nothing
+    if (g_dElapsedTime > 4.0) // wait for 3 seconds to switch to game mode, else do nothing
         g_eGameState = S_GAME;
 }
 
